64ce2c7544b2190c900fef94efea7d906774760d
reaganforonda <foronda.reagan@gmail.com>
2018-04-13 10:27:46 -0600

    completed Part 1 - Step 3

--- src/Component/Wizard/Wizard.js
+++ src/Component/Wizard/Wizard.js
@@ -5,8 +5,22 @@ export default class Wizard extends Component{
     constructor(props){
         super(props);
 
-        this.state = {}
+        this.state = {
+            name : '',
+            address : '',
+            city : '',
+            state : '',
+            zipcode : 0
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
+
+    // Update State based on input fields
+    handleInputChange(e){
+        this.setState({[e.target.name] : e.target.value})
+    }
+
     render(){
         return (
             <div>
@@ -14,6 +28,13 @@ export default class Wizard extends Component{
                 <Link to='/'>
                     <button>Cancel</button>
                 </Link>
+                    <div>
+                    <input onChange={(e)=>this.handleInputChange(e)} name='name' value={this.state.name} placeholder='Name'/>
+                    <input onChange={(e)=>this.handleInputChange(e)} name='address' value={this.state.address} placeholder='Address'/>
+                    <input onChange={(e)=>this.handleInputChange(e)} name='city' value={this.state.city} placeholder='City'/>
+                    <input onChange={(e)=>this.handleInputChange(e)} name='state' value={this.state.state} placeholder='State'/>
+                    <input onChange={(e)=>this.handleInputChange(e)} name='zipcode' value={this.state.zipcode} placeholder='Zipcode'/>
+                    </div>
             </div>
         )
     }
