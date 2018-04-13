7a0daacac628fce46e7ae87cfed97e0d64b30908
reaganforonda <foronda.reagan@gmail.com>
2018-04-13 10:05:45 -0600

    completed initial DB setup

--- server/index.js
+++ server/index.js
@@ -4,12 +4,18 @@ const controller = require('./controller');
 const cors = require('cors');
 const port = 4000;
 const app = express();
+const massive = require('massive');
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 app.use(bodyParser.json());
 app.use(cors());
 
 
+massive(process.env.CONNECTION_STRING).then((dbInstance) => {
+    app.set('db', dbInstance);
+}).catch((e) => console.log(`Error: ${e}`));
 
 app.listen(port, ()=> {
     console.log(`Creeping on Port: ${port}`);
